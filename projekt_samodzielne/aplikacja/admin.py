8497d19d441fe91f380248508f6ded03e377a50a
JakubArtysiewicz <jakub.artysiewicz@infotech.edu.pl>
2024-10-16 13:39:27 +0200

    Dodanie modelu, dodanie index.html itd

--- projekt_samodzielne/aplikacja/admin.py
+++ projekt_samodzielne/aplikacja/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(klucz_obc1)
+admin.site.register(model_1)
--- projekt_samodzielne/aplikacja/views.py
+++ projekt_samodzielne/aplikacja/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import model_1
 
 # Create your views here.
+def views_main(request):
+    dane_z_modelu = model_1.Objects.all(),
+    return(render(request,'index.html',context={"dane_z_modelu": dane_z_modelu}))
